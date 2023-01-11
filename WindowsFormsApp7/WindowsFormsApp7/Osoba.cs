cfef9422d7f4eab9d3bc9f3782e81f8f25d23d41
Ondra58 <97514734+Ondra58@users.noreply.github.com>
2023-01-11 20:32:15 +0100

    Add files via upload

---
+++ WindowsFormsApp7/WindowsFormsApp7/Osoba.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    internal class Osoba
+    {
+        protected string jmeno;
+        protected string prijmeni;
+        protected int hmotnost;
+        protected  double vyska;
+        public Osoba(string jmeno, string prijmeni, int hmotnost, double vyska)
+        {
+            this.Jmeno = jmeno;
+            this.Prijmeni = prijmeni;
+            this.Hmotnost = hmotnost;
+            this.Vyska = vyska;
+        }
+        public string Jmeno
+        {
+            get => jmeno;
+            set => jmeno = value;
+        }
+        public string Prijmeni
+        {
+            get => prijmeni;
+            set => prijmeni = value;
+        }
+        public int Hmotnost
+        {
+            get => hmotnost;
+            set
+            {
+                if (value > 0 && value <= 300)
+                {
+                    hmotnost = value;
+                }
+                else
+                {
+                    MessageBox.Show("Hmotnost mimo rozsah!");
+                }
+            }
+        }
+        public double Vyska
+        {
+            get => vyska;
+            set
+            {
+                if (value > 2.5)
+                {
+                    vyska = value / 100;
+                }
+                else
+                {
+                    vyska = value;
+                }
+            }
+        }
+        public virtual double BMI(int hmotnost, double vyska)
+        {
+            return (double)Math.Round((double)hmotnost / (vyska * vyska), 2);
+        }
+
+        public override string ToString()
+        {
+            return "Jméno: " + jmeno + "\nPříjmení: " + prijmeni + "\nHmotnost: " + hmotnost + "\nVýška: " + vyska + "\nBMI: " + BMI(hmotnost, vyska);
+        }
+    }
+}
---
+++ WindowsFormsApp7/WindowsFormsApp7/Pacient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp7
+{
+    internal class Pacient : Osoba
+    {
+        double obvodPasu;
+        public Pacient(string jmeno, string prijmeni, int hmotnost,double vyska, double obvodPasu) : base(jmeno, prijmeni, hmotnost, vyska)
+        {
+            this.ObvodPasu = obvodPasu;
+        }
+        public double ObvodPasu 
+        { 
+            get => obvodPasu;
+            set
+            {
+                if (value < 1.5)
+                {
+                    obvodPasu = value;
+                }
+                else
+                {
+                    obvodPasu = value / 100;
+                }
+            }
+        }
+        public double ABSI()
+        {
+            return (double)Math.Round(obvodPasu / (Math.Pow(BMI(hmotnost, vyska), 2 / 3) * Math.Pow(vyska, 1 / 2)), 3);
+        }
+        public override string ToString()
+        {
+            return base.ToString() + "\nObvod pasu: " + obvodPasu + "\nABSI: " + ABSI();
+        }
+    }
+}
